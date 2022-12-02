d9a736a2b247741a575cc580938f3e84a08f39c9
brian-yeo <briany@divergeIt.com>
2022-12-01 16:13:16 -0800

    Fetching movies with a traditional promise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 //index.js is the entry pt to the application
 
+//import movie data
+const movies = require("./data/movies.json");
+
+//import fetchWithTimeOut() from ./services
+import { fetchWithTimeout } from "./services";
+
 //declare fetchMovies()
-export function fetchMovies() {}
+export function fetchMovies() {
+  //create a resolve function called resolveFunction
+  const resolveFunction = () => movies;
+
+  //call and return fetchWithTimeout
+  return fetchWithTimeout(1000).then(resolveFunction);
+}
+
+//execute fetchMovies() Promise
+const moviePromise = fetchMovies();
+
+moviePromise.then((results) => {
+  console.log(results);
+});
