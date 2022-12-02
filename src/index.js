0f2871ebb44ed671d2cfb854ac38da1a98a29c84
brian-yeo <briany@divergeIt.com>
2022-12-01 16:36:58 -0800

    Use Promise.all() and Promise.race()

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 //index.js is the entry pt to the application
 //import fetchWithTimeOut() from ./services
-import { fetchWithTimeOut } from "./services";
+import { fetchWithTimeOut, fetchBooks, fetchMovies } from "./services";
 
+/*
 //import movie data
 const movies = require("./data/movies.json");
 
@@ -20,3 +21,30 @@ const moviePromise = fetchMovies();
 moviePromise.then((results) => {
   console.log(results);
 });
+*/
+
+function getBooksAndMovies() {
+  return Promise.all([fetchBooks(), fetchMovies()])
+    .then(([books, movies]) => ({
+      books,
+      movies,
+    }))
+    .catch((error) => console.log("Error fetching books and movies", error));
+}
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+
+//log results of getBooksAndMovies()
+getBooksAndMoviesPromise.then((results) => console.log("getBooksAndMoviesPromise", results));
+
+//fetch books OR movies with Promise.race()
+function getBooksOrMovies() {
+  return Promise.race([fetchBooks(), fetchMovies()])
+    .then((results) => results)
+    .catch((error) => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksOrMoviesPromise = getBooksAndMovies();
+
+//log results of getBooksOrMovies()
+getBooksOrMoviesPromise.then((results) => console.log("getBooksOrMoviesPromise", results));
--- src/services.js
+++ src/services.js
@@ -5,3 +5,20 @@ export function fetchWithTimeout(delay) {
   //add promise
   return new Promise((resolve) => setTimeout(resolve, delay));
 }
+
+export function fetchMovies() {
+  //use fetch api to get movies data from movies.json
+  return fetch("./data/movies.json")
+    .then((response) => response.json)
+    .then((movies) => movies)
+    .catch((error) => console.log(error));
+}
+
+// create another fetch method called fetchBooks
+export function fetchBooks() {
+  //use fetch api to get movies data from movies.json
+  return fetch("./data/books.json")
+    .then((response) => response.json)
+    .then((books) => books)
+    .catch((error) => console.log(error));
+}
